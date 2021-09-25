bb1fcfb9afab712d67bb9174b7f30defd6773fb6
Suryashankar Das <suryashankardas.2002@gmail.com>
2021-09-26 01:20:43 +0530

    contract: add signup function

--- contracts/Decentraskill.sol
+++ contracts/Decentraskill.sol
@@ -38,8 +38,10 @@ contract Decentraskill {
     struct skill {
         string name;
         bool verified;
-        certificate[] certifications;
-        endorsment[] endorsements;
+        uint256 num_certificates;
+        mapping(uint256 => certificate) certifications;
+        uint256 num_endorsements;
+        mapping(uint256 => endorsment) endorsements;
     }
 
     struct user {
@@ -48,7 +50,42 @@ contract Decentraskill {
         address wallet_address;
         bool is_employed;
         bool is_manager;
-        skill[] skills;
-        experience[] work_experience;
+        uint256 num_skill;
+        mapping(uint256 => skill) skills;
+        uint256 num_experience;
+        mapping(uint256 => experience) work_experience;
+    }
+
+    function memcmp(bytes memory a, bytes memory b)
+        internal
+        pure
+        returns (bool)
+    {
+        return (a.length == b.length) && (keccak256(a) == keccak256(b));
+    }
+
+    function strcmp(string memory a, string memory b)
+        internal
+        pure
+        returns (bool)
+    {
+        return memcmp(bytes(a), bytes(b));
+    }
+
+    function sign_up(string calldata email, string calldata acc_type) public {
+        require(email_to_address[email] == address(0));
+        email_to_address[email] = msg.sender;
+
+        if (strcmp(acc_type, "user")) {
+            user storage new_user = employees.push();
+            new_user.id = employees.length - 1;
+            new_user.wallet_address = msg.sender;
+        } else {
+            company storage new_company = companies.push();
+            new_company.id = companies.length - 1;
+            new_company.wallet_address = msg.sender;
+            new_company.current_employees = new uint256[](0);
+            new_company.previous_employees = new uint256[](0);
+        }
     }
 }
